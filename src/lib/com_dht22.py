08178fd1c9d1f898f122784b9547eb9dd974bce3
Bruno <delattreb@gmail.com>
2016-12-12 16:08:08 +0100

    Change DHT22 Driver

--- src/lib/com_dht22.py
+++ src/lib/com_dht22.py
@@ -33,7 +33,7 @@ class DHT22:
     gpio ------------+
     """
     
-    def __init__(self, gpio, led=None, power=None):
+    def __init__(self, gpio, led = None, power = None):
         """
         Instantiate with the Pi and gpio to which the DHT22 output
         pin is connected.
@@ -48,68 +48,68 @@ class DHT22:
         Taking readings more often than about once every two seconds will
         eventually cause the DHT22 to hang.  A 3 second interval seems OK.
         """
-        pi=pigpio.pi()
-        self.pi=pi
-        self.gpio=gpio
-        self.LED=led
-        self.power=power
+        pi = pigpio.pi()
+        self.pi = pi
+        self.gpio = gpio
+        self.LED = led
+        self.power = power
         
         if power is not None:
             self.pi.write(power, 1)  # Switch sensor on.
             time.sleep(2)
         
-        self.powered=True
+        self.powered = True
         
-        self.cb=None
+        self.cb = None
         
         atexit.register(self.cancel)
         
-        self.bad_CS=0  # Bad checksum count.
-        self.bad_SM=0  # Short message count.
-        self.bad_MM=0  # Missing message count.
-        self.bad_SR=0  # Sensor reset count.
+        self.bad_CS = 0  # Bad checksum count.
+        self.bad_SM = 0  # Short message count.
+        self.bad_MM = 0  # Missing message count.
+        self.bad_SR = 0  # Sensor reset count.
         
         # Power cycle if timeout > MAX_TIMEOUTS.
-        self.no_response=0
-        self.MAX_NO_RESPONSE=2
+        self.no_response = 0
+        self.MAX_NO_RESPONSE = 2
         
-        self.rhum=-999
-        self.temp=-999
+        self.rhum = -999
+        self.temp = -999
         
-        self.tov=None
+        self.tov = None
         
-        self.high_tick=0
-        self.bit=40
+        self.high_tick = 0
+        self.bit = 40
         
         self.pi.set_pull_up_down(gpio, pigpio.PUD_OFF)
         
         self.pi.set_watchdog(gpio, 0)  # Kill any watchdogs.
         
-        self.cb=self.pi.callback(gpio, pigpio.EITHER_EDGE, self._cb)
+        self.cb = self.pi.callback(gpio, pigpio.EITHER_EDGE, self._cb)
     
     def _cb(self, gpio, level, tick):
         """
         Accumulate the 40 data bits.  Format into 5 bytes, humidity high,
         humidity low, temperature high, temperature low, checksum.
         """
-        diff=pigpio.tickDiff(self.high_tick, tick)
+        diff = pigpio.tickDiff(self.high_tick, tick)
         
         if level == 0:
             
             # Edge length determines if bit is 1 or 0.
             
-            if diff>=50:
-                val=1
-                if diff>=200:  # Bad bit?
-                    self.CS=256  # Force bad checksum.
+            if diff >= 50:
+                val = 1
+                if diff >= 200:  # Bad bit?
+                    self.CS = 256  # Force bad checksum.
             else:
-                val=0
+                val = 0
             
-            if self.bit>=40:  # Message complete.
-                self.bit=40
+            if self.bit >= 40:  # Message complete.
+                self.bit = 40
             
-            elif self.bit>=32:  # In checksum byte.
-                self.CS=(self.CS<<1)+val
+            elif self.bit >= 32:  # In checksum byte.
+                self.CS = (self.CS << 1) + val
                 
                 if self.bit == 39:
                     
@@ -117,79 +117,79 @@ class DHT22:
                     
                     self.pi.set_watchdog(self.gpio, 0)
                     
-                    self.no_response=0
+                    self.no_response = 0
                     
-                    total=self.hH+self.hL+self.tH+self.tL
+                    total = self.hH + self.hL + self.tH + self.tL
                     
-                    if (total&255) == self.CS:  # Is checksum ok?
+                    if (total & 255) == self.CS:  # Is checksum ok?
                         
-                        self.rhum=((self.hH<<8)+self.hL)*0.1
+                        self.rhum = ((self.hH << 8) + self.hL) * 0.1
                         
-                        if self.tH&128:  # Negative temperature.
-                            mult=-0.1
-                            self.tH=self.tH&127
+                        if self.tH & 128:  # Negative temperature.
+                            mult = -0.1
+                            self.tH = self.tH & 127
                         else:
-                            mult=0.1
+                            mult = 0.1
                         
-                        self.temp=((self.tH<<8)+self.tL)*mult
+                        self.temp = ((self.tH << 8) + self.tL) * mult
                         
-                        self.tov=time.time()
+                        self.tov = time.time()
                         
                         if self.LED is not None:
                             self.pi.write(self.LED, 0)
                     
                     else:
                         
-                        self.bad_CS+=1
+                        self.bad_CS += 1
             
-            elif self.bit>=24:  # in temp low byte
-                self.tL=(self.tL<<1)+val
+            elif self.bit >= 24:  # in temp low byte
+                self.tL = (self.tL << 1) + val
             
-            elif self.bit>=16:  # in temp high byte
-                self.tH=(self.tH<<1)+val
+            elif self.bit >= 16:  # in temp high byte
+                self.tH = (self.tH << 1) + val
             
-            elif self.bit>=8:  # in humidity low byte
-                self.hL=(self.hL<<1)+val
+            elif self.bit >= 8:  # in humidity low byte
+                self.hL = (self.hL << 1) + val
             
-            elif self.bit>=0:  # in humidity high byte
-                self.hH=(self.hH<<1)+val
+            elif self.bit >= 0:  # in humidity high byte
+                self.hH = (self.hH << 1) + val
             
             else:  # header bits
                 pass
             
-            self.bit+=1
+            self.bit += 1
         
         elif level == 1:
-            self.high_tick=tick
-            if diff>250000:
-                self.bit=-2
-                self.hH=0
-                self.hL=0
-                self.tH=0
-                self.tL=0
-                self.CS=0
+            self.high_tick = tick
+            if diff > 250000:
+                self.bit = -2
+                self.hH = 0
+                self.hL = 0
+                self.tH = 0
+                self.tL = 0
+                self.CS = 0
         
         else:  # level == pigpio.TIMEOUT:
             self.pi.set_watchdog(self.gpio, 0)
-            if self.bit<8:  # Too few data bits received.
-                self.bad_MM+=1  # Bump missing message count.
-                self.no_response+=1
-                if self.no_response>self.MAX_NO_RESPONSE:
-                    self.no_response=0
-                    self.bad_SR+=1  # Bump sensor reset count.
+            if self.bit < 8:  # Too few data bits received.
+                self.bad_MM += 1  # Bump missing message count.
+                self.no_response += 1
+                if self.no_response > self.MAX_NO_RESPONSE:
+                    self.no_response = 0
+                    self.bad_SR += 1  # Bump sensor reset count.
                     if self.power is not None:
-                        self.powered=False
+                        self.powered = False
                         self.pi.write(self.power, 0)
                         time.sleep(2)
                         self.pi.write(self.power, 1)
                         time.sleep(2)
-                        self.powered=True
-            elif self.bit<39:  # Short message receieved.
-                self.bad_SM+=1  # Bump short message count.
-                self.no_response=0
+                        self.powered = True
+            elif self.bit < 39:  # Short message receieved.
+                self.bad_SM += 1  # Bump short message count.
+                self.no_response = 0
             
             else:  # Full message received.
-                self.no_response=0
+                self.no_response = 0
     
     def temperature(self):
         """Return current temperature."""
@@ -202,7 +202,7 @@ class DHT22:
     def staleness(self):
         """Return time since measurement made."""
         if self.tov is not None:
-            return time.time()-self.tov
+            return time.time() - self.tov
         else:
             return -999
     
@@ -240,7 +240,7 @@ class DHT22:
         
         if self.cb is not None:
             self.cb.cancel()
-            self.cb=None
+            self.cb = None
     
     def refreshlcd(self, lcd, cptread, delayread, cptws, delayws):
         # Display info on LCD
@@ -257,17 +257,17 @@ class DHT22:
         time.sleep(0.2)
         
         # Call WebServices
-        client=clientwebservices.ClientWebServices()
-        client.inserttemphum(name, self.temperature(), self.humidity())
+        client = clientwebservices.ClientWebServices()
+        client.inserttemphum(name, self.temperature()[:4], self.humidity()[:4])
     
     def set(self, name, connection, cursor):
         self.trigger()
         time.sleep(0.2)
         
         # Inssert into database
-        dal=dal_dht22.DAL_DHT22(connection, cursor)
+        dal = dal_dht22.DAL_DHT22(connection, cursor)
         dal.set_dht22(name, self.temperature(), self.humidity())
         
         # Log
-        logger=com_logger.Logger('DHT22')
-        logger.debug('Read Temp: '+str(self.temperature())[:4]+' Hum: '+str(self.humidity())[:4])
+        logger = com_logger.Logger('DHT22')
+        logger.debug('Read Temp: ' + str(self.temperature())[:4] + ' Hum: ' + str(self.humidity())[:4])
