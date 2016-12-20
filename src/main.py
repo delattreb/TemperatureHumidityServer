b7591c3f578958fe44a8bff90c0b09afbc70a3ef
Bruno <delattreb@gmail.com>
2016-12-20 22:43:27 +0100

    Correction on Database

--- src/main.py
+++ src/main.py
@@ -30,21 +30,13 @@ if int(config['LOGGER']['levelconsole']) > 10:
 # Create new threads
 threadlock = threading.Lock()
 
-# ds18b20_thread_int = thread_acquisition_ds18b20.ThreadAcquisitionDS18B20('DS18B20 Ext', threadlock, config['GPIO']['DS18B20_1'], float(config['GPIO'][
-#                                                                                                                                           'DS18B20_1_delay']),
-#                                                                         int(config['GPIO']['DS18B20_1_nb']))
-
-
 # TODO : Start pigpiod
 dht22_thread_int = thread_acquisition_dht22.ThreadAcquisitionDHT22(config['GPIO']['DHT22_INTERIOR_NAME'], threadlock, int(config['GPIO']['DHT22_INTERIOR_PORT']), int(config['GPIO']['DHT22_INTERIOR_delay']),
                                                                    int(config['GPIO']['DHT22_INTERIOR_delayread']), int(config['GPIO']['DHT22_INTERIOR_delayws']), int(config['GPIO']['DHT22_INTERIOR_nb']),
                                                                    int(config['GPIO']['LED_ACQUISITION']), True)
-
-# ds18b20_thread_int.start()
 dht22_thread_int.start()
 
 # Wait end for each thread
-# ds18b20_thread_int.join()
 dht22_thread_int.join()
 
 # logger.info('Application stop')
